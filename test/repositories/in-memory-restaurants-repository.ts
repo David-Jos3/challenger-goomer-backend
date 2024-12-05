3b0ad0257064c38bd38fec63f10b1e6f7d53d263
David-jos3 <davidgomesgc230@gmail.com>
2024-12-05 20:23:50 -0300

    test: add in-memory restaurant repository for unit testing

---
+++ test/repositories/in-memory-restaurants-repository.ts
@@ -0,0 +1,24 @@
+/* eslint-disable @stylistic/max-len */
+import { Restaurant } from '../../src/domain/entities/restaurant'
+import { RestaurantRepository } from '../../src/database/repositories/restaurant-repository.'
+
+export class InMemoryRestaurantsRepository implements RestaurantRepository {
+  private restaurants: Restaurant[] = []
+
+  async create(restaurant: Restaurant): Promise<Restaurant> {
+    this.restaurants.push(restaurant)
+    return restaurant
+  }
+
+  async findById(restaurantId: string): Promise<Restaurant | null> {
+    return this.restaurants.find((r) => r.id === restaurantId) ?? null
+  }
+
+  async findByName(name: string): Promise<Restaurant | null> {
+    return this.restaurants.find((r) => r.name === name) ?? null
+  }
+
+  async findByAddress(address: string): Promise<Restaurant | null> {
+    return this.restaurants.find((r) => r.address === address) ?? null
+  }
+}
