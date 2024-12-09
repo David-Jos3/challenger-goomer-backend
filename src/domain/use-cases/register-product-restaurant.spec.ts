ceeff7a2055b2177950e841de140837ec8df78e9
David-jos3 <davidgomesgc230@gmail.com>
2024-12-09 11:21:55 -0300

    test: add tests for RegisterProductRestaurant

---
+++ src/domain/use-cases/register-product-restaurant.spec.ts
@@ -0,0 +1,29 @@
+/* eslint-disable @stylistic/max-len */
+import { InMemoryProductRepository } from './../../../test/repositories/in-memory-product-repository'
+import { beforeEach, describe, test, expect } from 'vitest'
+import { RegisterProductRestaurantUseCase } from './register-product-restaurant'
+
+describe('Register Product the Restaurant', async () => {
+  let inMemoryProductRepository: InMemoryProductRepository
+  let sut: RegisterProductRestaurantUseCase
+
+  beforeEach(() => {
+    inMemoryProductRepository = new InMemoryProductRepository()
+    sut = new RegisterProductRestaurantUseCase(inMemoryProductRepository)
+  })
+
+  test('should be able to register one product the of restaurant', async () => {
+    const { product } = await sut.execute({
+      name: 'Test Product',
+      photoUrl: 'test-photo-url',
+      price: 10,
+      restaurantId: 'test-restaurant-id',
+      category: 'test-category',
+      createdAt: new Date(),
+    })
+    const storedProduct = await inMemoryProductRepository.findById(product.id)
+
+    expect(storedProduct).not.toBeNull()
+    expect(storedProduct!.name).toBe('Test Product')
+  })
+})
