49391ac4f4d842c874418de9f5929b7a301cb85f
David-jos3 <davidgomesgc230@gmail.com>
2024-12-11 10:14:39 -0300

    test: add validation for restaurant conflict with custom exception

--- src/domain/use-cases/register-restaurants.spec.ts
+++ src/domain/use-cases/register-restaurants.spec.ts
@@ -2,6 +2,7 @@
 import { InMemoryRestaurantsRepository } from './../../../test/repositories/in-memory-restaurants-repository'
 import { beforeEach, expect, test, describe } from 'vitest'
 import { RegisterRestaurantUseCase } from './register-restaurants'
+import { RestaurantConflictException } from './errors/restaurant-conflict-exception'
 
 let inMemoryRestaurantsRepository: InMemoryRestaurantsRepository
 let sut: RegisterRestaurantUseCase
@@ -18,9 +19,26 @@ describe('Resgister Restaurants', async () => {
       photoUrl: 'test-photo-url',
       createdAt: new Date(),
     })
-    const storedRestaurants = await inMemoryRestaurantsRepository.findById(restaurant.id)
-    expect(storedRestaurants).not.toBeNull()
-    expect(storedRestaurants!.name).toBe('Test Restaurant')
-    expect(storedRestaurants!.address).toBe('Test Address')
+    const result = await inMemoryRestaurantsRepository.findById(restaurant.id)
+    expect(result).not.toBeNull()
+    expect(result!.name).toBe('Test Restaurant')
+    expect(result!.address).toBe('Test Address')
+  })
+
+  test('should be able to not register restaurant same name iqual and adress', async () => {
+    await sut.execute({
+      name: 'Test Restaurant',
+      address: 'Test Address',
+      photoUrl: 'test-photo-url',
+      createdAt: new Date(),
+    })
+    await expect(
+      sut.execute({
+        name: 'Test Restaurant',
+        address: 'Test Address',
+        photoUrl: 'test-photo-url',
+        createdAt: new Date(),
+      }),
+    ).rejects.toThrow(RestaurantConflictException)
   })
 })
