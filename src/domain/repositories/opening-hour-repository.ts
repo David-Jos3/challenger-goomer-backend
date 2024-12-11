8fc5cb596f1659fda965322e30f80d272ae7ab78
David-jos3 <davidgomesgc230@gmail.com>
2024-12-11 10:26:37 -0300

    feat: add findById method to OpeningHourRepository

--- src/domain/repositories/opening-hour-repository.ts
+++ src/domain/repositories/opening-hour-repository.ts
@@ -2,4 +2,5 @@ import { OpeningHour } from '../../domain/entities/opening-hour'
 
 export interface OpeningHourRepository {
   create(openingHour: OpeningHour): Promise<OpeningHour>
+  findById(openingHourId: string): Promise<OpeningHour | null>
 }
