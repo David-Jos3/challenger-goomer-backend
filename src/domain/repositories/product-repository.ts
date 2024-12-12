944a594222544fc25a27256f821b967a176c7479
David-jos3 <davidgomesgc230@gmail.com>
2024-12-12 08:48:52 -0300

    feat: expand ProductRepository with new methods

--- src/domain/repositories/product-repository.ts
+++ src/domain/repositories/product-repository.ts
@@ -1,6 +1,9 @@
 import { Product } from '../../domain/entities/product'
 
 export interface ProductRepository {
-  create(product: Product): Promise<Product>
+  create(product: Product): Promise<void>
   findById(productId: string): Promise<Product | null>
+  findAll(): Promise<Product[]>
+  delete(productId: string): Promise<void>
+  update(product: Product): Promise<void>
 }
