9aae5b75324ba42b57512a0857d1a42ba40cd20f
David-jos3 <davidgomesgc230@gmail.com>
2024-12-12 08:49:29 -0300

    fix: change return type of create method to void

--- src/domain/repositories/opening-hour-repository.ts
+++ src/domain/repositories/opening-hour-repository.ts
@@ -1,6 +1,6 @@
 import { OpeningHour } from '../../domain/entities/opening-hour'
 
 export interface OpeningHourRepository {
-  create(openingHour: OpeningHour): Promise<OpeningHour>
+  create(openingHour: OpeningHour): Promise<void>
   findById(openingHourId: string): Promise<OpeningHour | null>
 }
