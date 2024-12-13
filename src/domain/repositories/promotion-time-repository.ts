08c104b8cccc0cc8ec5b8ed2dd8831eb75a8b378
David-jos3 <davidgomesgc230@gmail.com>
2024-12-13 12:03:26 -0300

    refactor: update create method to return void

--- src/domain/repositories/promotion-time-repository.ts
+++ src/domain/repositories/promotion-time-repository.ts
@@ -1,6 +1,6 @@
 import { PromotionTime } from '../../domain/entities/promotion-time'
 
 export interface PromotionTimeRepository {
-  create(promotionTime: PromotionTime) : Promise<PromotionTime>
+  create(promotionTime: PromotionTime) : Promise<void>
   findById(promotionTimeId: string) : Promise<PromotionTime | null>
 }
