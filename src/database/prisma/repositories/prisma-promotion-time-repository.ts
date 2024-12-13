15867a8aeb1199c0b528b5b6198e0f9e67c77358
David-jos3 <davidgomesgc230@gmail.com>
2024-12-13 11:58:44 -0300

    feat(repository): implement PrismaPromotionTimeRepository for promotion time CRUD operations

---
+++ src/database/prisma/repositories/prisma-promotion-time-repository.ts
@@ -0,0 +1,26 @@
+/* eslint-disable @stylistic/max-len */
+
+import { PromotionTimeRepository } from '../../../domain/repositories/promotion-time-repository'
+import { PromotionTime } from '../../../domain/entities/promotion-time'
+import { PrismaPromotionTimeMapper } from '../mappers/promotion-time-mapper'
+import { prisma } from '../../../lib/prisma-service'
+
+export class PrismaPromotionTimeRepository implements PromotionTimeRepository {
+  async create(promotionTime: PromotionTime): Promise<void> {
+    const data = PrismaPromotionTimeMapper.toPrisma(promotionTime)
+    prisma.promotionTime.create({
+      data,
+    })
+  }
+
+  async findById(promotionTimeId: string): Promise<PromotionTime | null> {
+    const promotionTime = await prisma.promotionTime.findUnique({
+      where: { id: promotionTimeId },
+      include: { product: true },
+    })
+    if (!promotionTime) {
+      return null
+    }
+    return PrismaPromotionTimeMapper.toDomain(promotionTime)
+  }
+}
