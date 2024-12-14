29b0f9b99fa2626860bd5d6dbc4a7fa5e5fbe114
David-jos3 <davidgomesgc230@gmail.com>
2024-12-14 10:43:31 -0300

    feat: configure query logging based on environment

--- src/lib/prisma-service.ts
+++ src/lib/prisma-service.ts
@@ -1,5 +1,8 @@
 import { PrismaClient } from '@prisma/client'
+import { env } from '../env/env'
 
 export const prisma = new PrismaClient({
-  log: ['query'],
+  log: env.NODE_ENV === 'dev'
+    ? ['query']
+    : [],
 })
