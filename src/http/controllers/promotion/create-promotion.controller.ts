97afedf894be9fae8b2c6a4e9468bf0f2337fa2a
David-jos3 <davidgomesgc230@gmail.com>
2024-12-15 17:06:34 -0300

    feat: add CreatePromotionTimeController for managing promotion times

---
+++ src/http/controllers/promotion/create-promotion.controller.ts
@@ -0,0 +1,39 @@
+/* eslint-disable @stylistic/max-len */
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+import { PrismaPromotionTimeRepository } from '../../../database/prisma/repositories/prisma-promotion-time-repository'
+import { CreatePromotionTimeUseCase } from '../../../domain/use-cases/create-promotion-time'
+
+const createPromotionTime = z.object({
+  dayOfWeek: z.string(),
+  startTime: z.string(),
+  endTime: z.string(),
+  price: z.number(),
+  description: z.string(),
+  productId: z.string(),
+})
+
+export class CreatePromotionTimeController {
+  static async create(request: FastifyRequest, reply: FastifyReply) {
+    try {
+      const { dayOfWeek, description, price, productId, startTime, endTime } = createPromotionTime.parse(request.body)
+
+      const promotionTimeRepository = new PrismaPromotionTimeRepository()
+      const createPromotionTimeUseCase = new CreatePromotionTimeUseCase(promotionTimeRepository)
+      await createPromotionTimeUseCase.execute({
+        dayOfWeek,
+        description,
+        endTime,
+        productId,
+        price,
+        startTime,
+      })
+    } catch (err) {
+      if (err instanceof z.ZodError) {
+        console.log(z.ZodError.toString)
+        return reply.status(400).send({ message: err.message })
+      }
+      reply.status(500).send({ message: 'Internal Server Error' })
+    }
+  }
+}
