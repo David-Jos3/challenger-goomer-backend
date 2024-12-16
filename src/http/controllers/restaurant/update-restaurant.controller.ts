295ddf405689157320a40f7442304fef24852e88
David-jos3 <davidgomesgc230@gmail.com>
2024-12-16 08:52:01 -0300

    feat: add UpdateRestaurantController with Zod validation and Prisma integration

---
+++ src/http/controllers/restaurant/update-restaurant.controller.ts
@@ -0,0 +1,36 @@
+/* eslint-disable @stylistic/max-len */
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z, ZodError } from 'zod'
+import { PrismaRestaurantRepository } from '../../../database/prisma/repositories/prisma-restaurant-repository'
+import { UpdateRestaurantUseCase } from '../../../domain/use-cases/update-restaurant'
+
+const updateRestaurantBodySchema = z.object({
+  restaurantId: z.string(),
+  name: z.string().optional(),
+  address: z.string().optional(),
+  photoUrl: z.string().optional(),
+})
+
+export class UpdateRestaurantController {
+  static async update(request:FastifyRequest, reply:FastifyReply): Promise<void> {
+    try {
+      const { restaurantId, address, name, photoUrl } = updateRestaurantBodySchema.parse(request.body)
+
+      const prismaRestaurantRepository = new PrismaRestaurantRepository()
+      const updateRestaurantUseCase = new UpdateRestaurantUseCase(prismaRestaurantRepository)
+
+      await updateRestaurantUseCase.execute({
+        address,
+        name,
+        photoUrl,
+        restaurantId,
+      })
+      return reply.status(204).send()
+    } catch (err) {
+      if (err instanceof ZodError) {
+        return reply.status(400).send({ message: err.message })
+      }
+      return reply.status(500).send({ message: 'Internal Server Error' })
+    }
+  }
+}
