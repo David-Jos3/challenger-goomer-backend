c90253f4537e2d91677279565082044bab0c9881
David-jos3 <davidgomesgc230@gmail.com>
2024-12-16 08:54:02 -0300

    refactor: enhance RestaurantRouter with CRUD routes

--- src/http/controllers/restaurant/router.ts
+++ src/http/controllers/restaurant/router.ts
@@ -1,8 +1,14 @@
 import { RegisterRestaurantController } from './register-restaurant.controller'
 import { app } from '../../../app'
+import { ListRestaurantController } from './list-restaurants.controller'
+import { DeleteRestaurantController } from './delete-restaurants.controller'
+import { UpdateRestaurantController } from './update-restaurant.controller'
 
-export class RegisterRestaurantRouter {
-  static async create() {
+export class RestaurantRouter {
+  static async routes() {
     app.post('/restaurants', RegisterRestaurantController.create)
+    app.get('/restaurants', ListRestaurantController.findAll)
+    app.delete('/restaurants/:id', DeleteRestaurantController.delete)
+    app.put('/restaurants/:id', UpdateRestaurantController.update)
   }
 }
