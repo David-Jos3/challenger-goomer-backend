e8e7cff0d2b99cb23a2a27d3d82cdaad955e8a51
David-jos3 <davidgomesgc230@gmail.com>
2024-12-18 09:59:49 -0300

    feat: add relations to PrismaRestaurantMapper for products and opening hours

--- src/database/prisma/mappers/restaurant-mapper.ts
+++ src/database/prisma/mappers/restaurant-mapper.ts
@@ -1,14 +1,36 @@
 /* eslint-disable @stylistic/max-len */
-import { Restaurant as PrismaRestaurant, Prisma } from '@prisma/client'
+import { Restaurant as PrismaRestaurant, Product as PrismaProduct, OpeningHour as PrismaOpeningHour, Prisma } from '@prisma/client'
 import { Restaurant } from '../../../domain/entities/restaurant'
 
+type PrismaRestaurantWithRelations = PrismaRestaurant & {
+  Product: PrismaProduct[];
+  OpeningHour: PrismaOpeningHour[];
+}
+
 export class PrismaRestaurantMapper {
-  static toDomain(raw: PrismaRestaurant): Restaurant {
+  static toDomain(raw: PrismaRestaurantWithRelations): Restaurant {
     return new Restaurant({
       name: raw.name,
       address: raw.address,
       photoUrl: raw.photoUrl,
       createdAt: raw.createdAt,
+      updatedAt: raw.updatedAt,
+      product: raw.Product.map(products => ({
+        id: products.id.toString(),
+        name: products.name,
+        photoUrl: products.photoUrl,
+        restaurantId: raw.id,
+        promotionTime: [],
+        price: products.price.toNumber(),
+        category: products.category,
+        createdAt: products.createdAt,
+      })),
+      openingHours: raw.OpeningHour.map(openingHour => ({
+        dayOfWeek: openingHour.dayOfWeek,
+        endTime: openingHour.endTime,
+        startTime: openingHour.startTime,
+        restaurantId: raw.id,
+      })),
     }, raw.id)
   }
 
