33d52cd1c6a6d1d07fc41f3f459b89016512392a
David-jos3 <davidgomesgc230@gmail.com>
2024-12-18 10:09:33 -0300

    feat: add CRUD routes for product restaurant management

--- src/http/controllers/product/router..ts
+++ src/http/controllers/product/router..ts
@@ -1,9 +1,17 @@
 /* eslint-disable @stylistic/max-len */
 import { app } from '../../../app'
+import { DeleteProductRestaurantController } from './delete-product-restaurant.controller'
+import { ListProductRestaurantByIdController } from './list-product-restaurant-by-id.controller'
+import { ListProductRestaurantController } from './list-product-restaurant.controller'
 import { RegisterProductRestaurantsController } from './register-product-restaurant.controller'
+import { UpdateProductRestaurantController } from './update-product-restaurant.controller'
 
 export class ProductRestaurantRouter {
   static async routes() {
     app.post('/products', RegisterProductRestaurantsController.create)
+    app.get('/products', ListProductRestaurantController.findAll)
+    app.get('/products/:id', ListProductRestaurantByIdController.findById)
+    app.put('/products/:id', UpdateProductRestaurantController.update)
+    app.delete('/products/:id', DeleteProductRestaurantController.delete)
   }
 }
