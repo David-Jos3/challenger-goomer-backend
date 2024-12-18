894f84a7fffa8334ae9713deac28e1ebe3dda417
David-jos3 <davidgomesgc230@gmail.com>
2024-12-18 10:13:00 -0300

    fix: update route to use CreatePromotionTimeController instead of CreateOpeningHourController in PromotionTimeRouter

--- src/http/controllers/promotion/router.ts
+++ src/http/controllers/promotion/router.ts
@@ -1,9 +1,9 @@
 import { app } from '../../../app'
-// eslint-disable-next-line @stylistic/max-len
-import { CreateOpeningHourController } from '../create-opening-hour/create-opening-hour.controller'
+
+import { CreatePromotionTimeController } from './create-promotion.controller'
 
 export class PromotionTimeRouter {
   static async routes() {
-    app.post('/promotion-time', CreateOpeningHourController.create)
+    app.post('/promotion-time', CreatePromotionTimeController.create)
   }
 }
