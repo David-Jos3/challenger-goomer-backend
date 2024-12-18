24384d8a4dfb530080b9d57c8a83b3c00be49187
David-jos3 <davidgomesgc230@gmail.com>
2024-12-18 09:57:54 -0300

    feat: register routers for restaurant, product, opening hour, and promotion routes

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,19 @@
+/* eslint-disable @stylistic/max-len */
 import fastify from 'fastify'
 import { ZodError } from 'zod'
 import { env } from './env/env'
+import { RestaurantRouter } from './http/controllers/restaurant/router'
+import { ProductRestaurantRouter } from './http/controllers/product/router.'
+import { OpeningHourRouter } from './http/controllers/create-opening-hour/router'
+import { PromotionTimeRouter } from './http/controllers/promotion/router'
 
 export const app = fastify({
   logger: true,
 })
-
-app.get('/', async (req, res) => {
-  res.send({ hello: 'world' })
-})
+app.register(RestaurantRouter.routes)
+app.register(ProductRestaurantRouter.routes)
+app.register(OpeningHourRouter.routes)
+app.register(PromotionTimeRouter.routes)
 
 app.setErrorHandler((error, _request, reply) => {
   if (error instanceof ZodError) {
