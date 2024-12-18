7d6311a668e6de0a0d5937147d0ac1b7e0b561ef
David-jos3 <davidgomesgc230@gmail.com>
2024-12-18 10:13:32 -0300

    fix: update DeleteRestaurantController to use request.params for restaurantId

--- src/http/controllers/restaurant/delete-restaurants.controller.ts
+++ src/http/controllers/restaurant/delete-restaurants.controller.ts
@@ -4,22 +4,26 @@ import { PrismaRestaurantRepository } from '../../../database/prisma/repositorie
 import { DeleteRestaurantUseCase } from '../../../domain/use-cases/delete-restaurant'
 import { FastifyReply, FastifyRequest } from 'fastify'
 
-const deleteRestaurantBodySchema = z.object({
-  restaurantId: z.string(),
+const deleteRestaurantParamsSchema = z.object({
+  id: z.string(),
 })
 
 export class DeleteRestaurantController {
   static async delete(request: FastifyRequest, reply: FastifyReply) :Promise<void> {
     try {
-      const { restaurantId } = deleteRestaurantBodySchema.parse(request.body)
+      const { id } = deleteRestaurantParamsSchema.parse(request.params)
 
       const restaurantRepository = new PrismaRestaurantRepository()
       const deleteRestaurantUseCase = new DeleteRestaurantUseCase(restaurantRepository)
 
-      await deleteRestaurantUseCase.execute({ restaurantId })
+      await deleteRestaurantUseCase.execute({ restaurantId: id })
+
       reply.status(204).send()
     } catch (err) {
       if (err instanceof z.ZodError) {
+        return reply.status(400).send({ message: err.message })
+      }
+      if (err instanceof Error) {
         return reply.status(400).send({ message: err.message })
       }
       reply.status(500).send({ message: 'Internal Server Error' })
