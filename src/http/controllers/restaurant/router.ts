7e3d783c6040bf257d9be767e715fb35b6ef4867
David-jos3 <davidgomesgc230@gmail.com>
2024-12-18 09:57:00 -0300

    feat: add route to find restaurant by id

--- src/http/controllers/restaurant/router.ts
+++ src/http/controllers/restaurant/router.ts
@@ -1,8 +1,10 @@
+/* eslint-disable @stylistic/max-len */
 import { RegisterRestaurantController } from './register-restaurant.controller'
 import { app } from '../../../app'
 import { ListRestaurantController } from './list-restaurants.controller'
 import { DeleteRestaurantController } from './delete-restaurants.controller'
 import { UpdateRestaurantController } from './update-restaurant.controller'
+import { ListRestaurantByIdController } from './list-restaurant-by-id.controller'
 
 export class RestaurantRouter {
   static async routes() {
@@ -10,5 +12,6 @@ export class RestaurantRouter {
     app.get('/restaurants', ListRestaurantController.findAll)
     app.delete('/restaurants/:id', DeleteRestaurantController.delete)
     app.put('/restaurants/:id', UpdateRestaurantController.update)
+    app.get('/restaurants/:id', ListRestaurantByIdController.findById)
   }
 }
